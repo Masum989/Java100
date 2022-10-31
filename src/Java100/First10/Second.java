2da2aafb46b79b58d947118e5cb5287a8aa80f70
masum <masumraza510@gmail.com>
2022-10-31 10:45:12 +0530

    This is second  commit

---
+++ src/Java100/First10/Second.java
@@ -0,0 +1,8 @@
+package Java100.First10;
+// print integer in java
+public class Second {
+    public static void main(String[] args) {
+        int  a= 10;
+        System.out.println(a);
+    }
+}
