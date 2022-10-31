ea8c905c07f7c2df6bfcfd1e6af8cda3e3f49891
masum <masumraza510@gmail.com>
2022-10-31 10:48:07 +0530

    This is third   commit

---
+++ src/Java100/First10/Third.java
@@ -0,0 +1,12 @@
+package Java100.First10;
+
+import java.util.Scanner;
+
+// get input form user
+public class Third {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
