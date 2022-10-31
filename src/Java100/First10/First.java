876f735d40f42616453e3b4bd4715dbe484ad3f1
masum <masumraza510@gmail.com>
2022-10-31 10:41:55 +0530

    This is first commit

--- src/Java100/First10/First.java
+++ src/Java100/First10/First.java
@@ -3,5 +3,6 @@ package Java100.First10;
 public class First {
     public static void main(String[] args) {
         System.out.println("This is first java project");
+        System.out.println(" This is second changes in this file");
     }
 }
