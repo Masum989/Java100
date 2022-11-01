0ac2feb08afdc0591d2c176f40a9b8e500e9d477
masum <masumraza510@gmail.com>
2022-11-01 21:17:59 +0530

    This is seventh commit

---
+++ src/Java100/First10/Seventh.java
@@ -0,0 +1,14 @@
+package Java100.First10;
+// add two number in java
+
+import java.util.Scanner;
+
+public class Seventh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
