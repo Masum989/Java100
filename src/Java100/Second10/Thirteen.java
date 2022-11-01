7e8ad6792938da14c54b319255337405139a1fa8
masum <masumraza510@gmail.com>
2022-11-01 21:55:13 +0530

    This is thirteen   commit

---
+++ src/Java100/Second10/Thirteen.java
@@ -0,0 +1,14 @@
+package Java100.Second10;
+
+import java.util.Scanner;
+
+// simple for loop
+public class Thirteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =0;i<n;i++){
+            System.out.println(i);
+        }
+    }
+}
