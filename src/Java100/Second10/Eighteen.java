47ce4fc9b7aecb88031648da25a1548f4804128b
masum <masumraza510@gmail.com>
2022-11-06 11:45:43 +0530

    This is Eighteen   commit

---
+++ src/Java100/Second10/Eighteen.java
@@ -0,0 +1,9 @@
+package Java100.Second10;
+// print all the alphabet using for loop
+public class Eighteen {
+    public static void main(String[] args) {
+        char ch;
+        for(ch ='a';ch<='z';ch++)
+            System.out.println(ch);
+    }
+}
